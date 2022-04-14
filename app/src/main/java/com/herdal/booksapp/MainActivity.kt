68e79093c1453b76c10c9e582a51f2c3b483ee99
herdal06 <h.erdal721@gmail.com>
2022-04-15 01:27:20 +0300

    Fix RecyclerView size and Add toolbar title

--- app/src/main/java/com/herdal/booksapp/MainActivity.kt
+++ app/src/main/java/com/herdal/booksapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.herdal.booksapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbarCategory.title = "Categories"
+        setSupportActionBar(toolbarCategory)
+
+        recyclerViewCategories.setHasFixedSize(true)
+        recyclerViewCategories.layoutManager = LinearLayoutManager(this)
     }
 }
