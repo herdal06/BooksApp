f0c77d80ee424f55007e5d322176ce22e236b8f4
herdal06 <h.erdal721@gmail.com>
2022-04-15 02:24:33 +0300

    Fix RecyclerView size and Add toolbar title for books

--- app/src/main/java/com/herdal/booksapp/view/BooksActivity.kt
+++ app/src/main/java/com/herdal/booksapp/view/BooksActivity.kt
@@ -2,11 +2,21 @@ package com.herdal.booksapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.herdal.booksapp.R
+import kotlinx.android.synthetic.main.activity_books.*
 
 class BooksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_books)
+
+        toolbarBooks.title = "Books"
+        setSupportActionBar(toolbarBooks)
+
+        recyclerViewBooks.setHasFixedSize(true)
+        recyclerViewBooks.layoutManager = StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
+
     }
 }
