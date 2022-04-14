9d2adfd2543b39d191bca37cadd8f640bb4a04a6
herdal06 <h.erdal721@gmail.com>
2022-04-15 02:43:13 +0300

    Design film details page

---
+++ app/src/main/java/com/herdal/booksapp/view/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.herdal.booksapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.herdal.booksapp.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
