e90b8239ed4d3009d958863a707775370c4cc57a
herdal06 <h.erdal721@gmail.com>
2022-04-15 01:09:53 +0300

    Create Author and Category data classes

---
+++ app/src/main/java/com/herdal/booksapp/model/Author.kt
@@ -0,0 +1,6 @@
+package com.herdal.booksapp.model
+
+import java.io.Serializable
+
+data class Author(var id: Int, var name: String) : Serializable {
+}
---
+++ app/src/main/java/com/herdal/booksapp/model/Category.kt
@@ -0,0 +1,6 @@
+package com.herdal.booksapp.model
+
+import java.io.Serializable
+
+class Category(var id: Int, var name: String) : Serializable {
+}
