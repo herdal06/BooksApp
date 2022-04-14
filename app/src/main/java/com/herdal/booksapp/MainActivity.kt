e23d7c7ea0775cb60dda45ca774d683eaa68a16a
herdal06 <h.erdal721@gmail.com>
2022-04-15 01:33:19 +0300

    Define category list and adapter

--- app/src/main/java/com/herdal/booksapp/MainActivity.kt
+++ app/src/main/java/com/herdal/booksapp/MainActivity.kt
@@ -3,9 +3,15 @@ package com.herdal.booksapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.herdal.booksapp.model.Category
+import com.herdal.booksapp.service.CategoryAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var categoryList: ArrayList<Category>
+    private lateinit var categoryAdapter: CategoryAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,5 +21,8 @@ class MainActivity : AppCompatActivity() {
 
         recyclerViewCategories.setHasFixedSize(true)
         recyclerViewCategories.layoutManager = LinearLayoutManager(this)
+
+        categoryList = ArrayList()
+
     }
 }
